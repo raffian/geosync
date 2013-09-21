dddc050b350e634d743c01b8a9cac14396ad3ecb
Raffi Basmajian <raffi@raffianphoto.com>
2013-09-21 16:46:52 -0400

    initial

---
+++ src/com/raffian/geosync/Queue.groovy
@@ -0,0 +1,29 @@
+package com.raffian.geosync
+
+import org.zeromq.ZContext
+import org.zeromq.ZMQ
+import org.zeromq.ZThread
+import org.zeromq.ZMQ.Socket
+import org.zeromq.ZThread.IAttachedRunnable
+import org.zmq.base.GeoSyncProxy
+
+import com.raffian.geosync.util.FrameListener
+
+
+class Queue extends GeoSyncProxy implements IAttachedRunnable {
+
+	public Queue(){
+		clazzname = this.class.simpleName
+	}
+	
+	@Override
+	public void run(Object[] args, ZContext ctx, Socket pipe) {								
+      Socket input = ctx.createSocket(ZMQ.ROUTER)
+		input.bind( proxyIn)
+		Socket output = ctx.createSocket(ZMQ.DEALER)
+		output.bind( proxyOut)
+			
+		log("proxy started")
+		ZMQ.proxy( input, output, ZThread.fork(ctx, new FrameListener(clazzname)))			
+	}
+}
